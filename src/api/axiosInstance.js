a96569b9b32dd56d2b783e009a86b6a9457fe238
Subhadeep Ghosh <subhadeep051@icloud.com>
2025-10-05 19:01:15 +0530

    UI Upgradation

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_BACKEND_URL || "http://localhost:5001/api",
   withCredentials: true,
 });
 
